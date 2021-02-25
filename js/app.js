88362d206fb2d551d4c7e7a9f04166cd7365d694
lymbus <lymbus@users.noreply.github.com>
2021-02-25 09:06:03 +0100

    SegundaVersión
    -Mejoras

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@ var nav = document.querySelector('.header__nav');
 var menu = document.querySelector('.header');
 var barra = document.querySelector('.header__menu');
 var liMenu = document.querySelectorAll('.header__li');
-var proy = document.querySelector('.proyectos__tipo');
-var flechas = document.querySelectorAll('.proyectos__flecha');
+var proy = document.querySelector('.resumen__tipo');
 var contacto = document.querySelector('.contacto__pop');
 var bContacto = document.querySelector('.contacto__p');
 var bCerrar = document.querySelector('.contacto__cerrar');
-var sCerrar = document.querySelector('.proyectos__cerrar');
-var imagenes = document.querySelectorAll('.proyectos__img');
+var sCerrar = document.querySelector('.resumen__cerrar');
+var imagenes = document.querySelectorAll('.resumen__img');
+var icono = document.querySelectorAll('.proyectos__icono');
+var git = document.querySelectorAll('.proyectos__git');
 // Abrir/cerrar CONTACTO
 var abrir = function () {
     menu.classList.toggle('ampliar');
@@ -46,50 +47,73 @@ window.addEventListener('scroll', function () {
     }
 });
 // CREAR 'COMPONENTE'-->PROYECTO
-var aGit = document.querySelectorAll('.web');
-var ulProyecto = document.querySelectorAll('.proyectos__ul');
-var fragmento = document.createDocumentFragment();
-ulProyecto.forEach(function (cadaImg, i) {
-    var seccion = document.createElement('div');
-    seccion.classList.add('proyectos__seccion');
-    seccion.appendChild(ulProyecto[i]);
-    var alt = aGit[i].getAttribute('title');
-    var h3 = document.createElement('h3');
-    h3.classList.add('proyectos__h3');
-    h3.innerText = alt;
-    seccion.appendChild(h3);
-    fragmento.appendChild(seccion);
-});
-proy.appendChild(fragmento);
-var seccionProy = document.querySelectorAll('.proyectos__seccion');
-seccionProy.forEach(function (cadaP, i) {
-    seccionProy[i].addEventListener('click', function () {
-        seccionProy[i].classList.toggle('acordeon');
-    });
-});
+// const aGit= document.querySelectorAll<HTMLElement>    ('.web')
+// const ulProyecto= document.querySelectorAll<HTMLElement>    ('.proyectos__ul')
+// let fragmento=(document.createDocumentFragment() as DocumentFragment)
+// ulProyecto.forEach((cadaImg:any,i:number):void=>{
+//       let seccion=   (document.createElement('div') as HTMLElement)  
+//       seccion.classList.add('proyectos__seccion') 
+//       seccion.appendChild(ulProyecto[i])
+//       let alt:string= aGit[i].getAttribute('title')
+//       let h3=(document.createElement('h3')as HTMLElement)
+//       h3.classList.add('proyectos__h3')
+//       h3.innerText=alt
+//       seccion.appendChild(h3)
+//       fragmento.appendChild(seccion)    
+// })
+// proy.appendChild(fragmento) 
+// const seccionProy=   document.querySelectorAll <HTMLElement>('.proyectos__seccion')
+// seccionProy.forEach((cadaP:any,i:number):void=>{
+//       seccionProy[i].addEventListener('click',():void=>{
+//             seccionProy[i].classList.toggle('acordeon')
+//       })
+// })
 // Slider
-var foto = 0;
-var pasarFoto = function () {
-    imagenes.forEach(function (cadaImg, i) {
-        imagenes.forEach(function (cadaImg, i) {
-            imagenes[i].classList.remove('ver');
+// let foto:number=0
+// const pasarFoto=():void=>{ 
+//       imagenes.forEach((cadaImg :any,i:number)=>{
+//           imagenes.forEach((cadaImg,i):void=>{
+//               imagenes[i].classList.remove('ver')
+//           })
+//       imagenes[foto].classList.add('ver') 
+//   })
+//   }
+//   const siguienteFoto= ():void =>{
+//       foto++ 
+//       if(foto===imagenes.length){
+//           foto=0
+//       }
+//       pasarFoto()
+//   }
+//   const anteriorFoto=() :void =>{   
+//       if(foto===0){
+//           foto=imagenes.length
+//       }
+//       foto--
+//       pasarFoto()
+//   }
+//   flechas[1].addEventListener('click',anteriorFoto)
+//   flechas[0].addEventListener('click',siguienteFoto)
+//   LINEAS MENÚ
+var pincel = document.querySelectorAll('.header__svg');
+liMenu.forEach(function (cadaLista, i) {
+    liMenu[i].addEventListener('click', function () {
+        pincel.forEach(function (cadaLista, i) {
+            pincel[i].classList.remove('des');
         });
-        imagenes[foto].classList.add('ver');
+        pincel[i].classList.add('des');
     });
-};
-var siguienteFoto = function () {
-    foto++;
-    if (foto === imagenes.length) {
-        foto = 0;
-    }
-    pasarFoto();
-};
-var anteriorFoto = function () {
-    if (foto === 0) {
-        foto = imagenes.length;
-    }
-    foto--;
-    pasarFoto();
-};
-flechas[1].addEventListener('click', anteriorFoto);
-flechas[0].addEventListener('click', siguienteFoto);
+});
+var seccionProy = document.querySelectorAll('.proyectos__seccion');
+icono.forEach(function (cadaP, i) {
+    icono[i].addEventListener('click', function () {
+        git.forEach(function (cadaP, i) {
+            git[i].classList.remove('play');
+            icono[i].style.opacity = '1';
+        });
+    });
+    icono[i].addEventListener('click', function () {
+        git[i].classList.add('play');
+        icono[i].style.opacity = '0';
+    });
+});
--- ts/app.ts
+++ ts/app.ts
@@ -4,14 +4,18 @@ const nav    = ( document.querySelector ('.header__nav')  as HTMLElement )
 const menu   = ( document.querySelector ('.header')       as HTMLElement )
 const barra  = ( document.querySelector ('.header__menu') as HTMLElement )
 const liMenu =   document.querySelectorAll <HTMLElement>   ('.header__li')
-const proy=( document.querySelector ('.proyectos__tipo') as HTMLElement )
-const flechas=document.querySelectorAll<HTMLImageElement>('.proyectos__flecha')
+const proy=( document.querySelector ('.resumen__tipo') as HTMLElement )
+
 
 const contacto   = ( document.querySelector ('.contacto__pop')    as HTMLElement )
 const bContacto  = ( document.querySelector ('.contacto__p')      as HTMLElement )
 const bCerrar    = ( document.querySelector ('.contacto__cerrar') as HTMLElement )
-const sCerrar    = ( document.querySelector ('.proyectos__cerrar') as HTMLElement )
-const imagenes   =   document.querySelectorAll <HTMLElement>   ('.proyectos__img')
+const sCerrar    = ( document.querySelector ('.resumen__cerrar') as HTMLElement )
+const imagenes   =   document.querySelectorAll <HTMLElement>   ('.resumen__img')
+
+const icono      =   document.querySelectorAll <HTMLElement>   ('.proyectos__icono')
+
+const git        =   document.querySelectorAll <HTMLElement>   ('.proyectos__git')
 
 // Abrir/cerrar CONTACTO
 let abrir=():void=>{
@@ -45,9 +49,12 @@ liMenu.forEach((cadaLi:any,i:number):void =>{
 // Abrir MENÚ
 barra.addEventListener('click',abrir)
 
+
+
+
 window.addEventListener('scroll',():void=>{    
             if(window.scrollY>500 ){
-                  menu.classList.add('ampliar')           
+                  menu.classList.add('ampliar') 
             }
             else{
                   menu.classList.remove('ampliar')
@@ -55,59 +62,88 @@ window.addEventListener('scroll',():void=>{
 })
                    
 // CREAR 'COMPONENTE'-->PROYECTO
-const aGit= document.querySelectorAll<HTMLElement>    ('.web')
-const ulProyecto= document.querySelectorAll<HTMLElement>    ('.proyectos__ul')
+// const aGit= document.querySelectorAll<HTMLElement>    ('.web')
+// const ulProyecto= document.querySelectorAll<HTMLElement>    ('.proyectos__ul')
 
-let fragmento=(document.createDocumentFragment() as DocumentFragment)
+// let fragmento=(document.createDocumentFragment() as DocumentFragment)
 
-ulProyecto.forEach((cadaImg:any,i:number):void=>{
-      let seccion=   (document.createElement('div') as HTMLElement)  
-      seccion.classList.add('proyectos__seccion') 
-      seccion.appendChild(ulProyecto[i])
+// ulProyecto.forEach((cadaImg:any,i:number):void=>{
+//       let seccion=   (document.createElement('div') as HTMLElement)  
+//       seccion.classList.add('proyectos__seccion') 
+//       seccion.appendChild(ulProyecto[i])
             
-      let alt:string= aGit[i].getAttribute('title')
-      let h3=(document.createElement('h3')as HTMLElement)
-      h3.classList.add('proyectos__h3')
-      h3.innerText=alt
-      seccion.appendChild(h3)
-      fragmento.appendChild(seccion)    
-})
-proy.appendChild(fragmento) 
-const seccionProy=   document.querySelectorAll <HTMLElement>('.proyectos__seccion')
+//       let alt:string= aGit[i].getAttribute('title')
+//       let h3=(document.createElement('h3')as HTMLElement)
+//       h3.classList.add('proyectos__h3')
+//       h3.innerText=alt
+//       seccion.appendChild(h3)
+//       fragmento.appendChild(seccion)    
+// })
+// proy.appendChild(fragmento) 
+// const seccionProy=   document.querySelectorAll <HTMLElement>('.proyectos__seccion')
 
 
-seccionProy.forEach((cadaP:any,i:number):void=>{
-      seccionProy[i].addEventListener('click',():void=>{
-            seccionProy[i].classList.toggle('acordeon')
-      })
-})
+// seccionProy.forEach((cadaP:any,i:number):void=>{
+//       seccionProy[i].addEventListener('click',():void=>{
+//             seccionProy[i].classList.toggle('acordeon')
+//       })
+// })
 
 // Slider
-let foto:number=0
-const pasarFoto=():void=>{ 
-      imagenes.forEach((cadaImg :any,i:number)=>{
-          imagenes.forEach((cadaImg,i):void=>{
-              imagenes[i].classList.remove('ver')
-          })
-      imagenes[foto].classList.add('ver') 
-  })
-  }
-  const siguienteFoto= ():void =>{
-      foto++ 
-      if(foto===imagenes.length){
-          foto=0
-      }
-      pasarFoto()
-  }
+// let foto:number=0
+// const pasarFoto=():void=>{ 
+//       imagenes.forEach((cadaImg :any,i:number)=>{
+//           imagenes.forEach((cadaImg,i):void=>{
+//               imagenes[i].classList.remove('ver')
+//           })
+//       imagenes[foto].classList.add('ver') 
+//   })
+//   }
+//   const siguienteFoto= ():void =>{
+//       foto++ 
+//       if(foto===imagenes.length){
+//           foto=0
+//       }
+//       pasarFoto()
+//   }
   
-  const anteriorFoto=() :void =>{   
-      if(foto===0){
-          foto=imagenes.length
-      }
-      foto--
-      pasarFoto()
-  }
+//   const anteriorFoto=() :void =>{   
+//       if(foto===0){
+//           foto=imagenes.length
+//       }
+//       foto--
+//       pasarFoto()
+//   }
   
-  flechas[1].addEventListener('click',anteriorFoto)
+//   flechas[1].addEventListener('click',anteriorFoto)
 
-  flechas[0].addEventListener('click',siguienteFoto)
+//   flechas[0].addEventListener('click',siguienteFoto)
+
+//   LINEAS MENÚ
+
+const pincel=document.querySelectorAll<HTMLElement>('.header__svg')
+
+liMenu.forEach((cadaLista,i):void =>{
+      liMenu[i].addEventListener('click',():void =>{
+          pincel.forEach((cadaLista,i): void =>{
+              pincel[i].classList.remove('des')
+          })
+      pincel[i].classList.add('des')    
+      })
+  })
+
+const seccionProy=document.querySelectorAll<HTMLElement>('.proyectos__seccion');
+
+icono.forEach((cadaP:any,i:number):void=>{
+      icono[i].addEventListener('click',():void=>{
+            git.forEach((cadaP:any,i:number):void=>{
+                  git[i].classList.remove('play')
+                  icono[i].style.opacity='1' 
+            })
+      })
+            icono[i].addEventListener('click',():void=>{
+                  git[i].classList.add('play')
+                  icono[i].style.opacity='0'
+            })
+})
+
